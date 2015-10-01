fc823fe421ca4126a7271ae06821a8205778cbb6
hackstutz <rudi@farys.de>
2015-10-02 01:26:03 +0200

    data setup

---
+++ data_setup.r
@@ -0,0 +1,17 @@
+library(data.table)
+library(xlsx)
+#poverty_orig <- as.data.table(read.table("C:/Users/Hackstutz/Dropbox/Git/bern_poverty/poverty.csv",header=TRUE,sep=";"))
+poverty_orig <- as.data.table(read.xlsx("C:/Users/Hackstutz/Dropbox/Git/armutsquoten/Poverty Tables/poverty.xlsx",1))
+
+poverty_orig[,relPov:=relPov1/(relPov1+relPov0)]
+poverty_orig[,pov_rel_municip:=pov_rel_municip1/(pov_rel_municip1+pov_rel_municip0)]
+poverty_orig[,absPov:=absPov1/(absPov1+absPov0)]
+poverty_orig[,nHH:=relPov1+relPov0]
+poverty_orig[nHH<50,c("relPov","absPov","pov_rel_municip"):=NA,with=FALSE]
+
+
+write.table(poverty_orig,file="C:/Users/Hackstutz/Dropbox/Git/armutsquoten/poverty.tsv",sep="\t",row.names = FALSE)
+write.table(poverty_orig,file="C:/Users/Hackstutz/Dropbox/Git/armutsquoten/poverty.csv",sep=",",row.names = FALSE)
+
+
+poverty <- read.table("C:/Users/Hackstutz/Dropbox/Git/bern_poverty/poverty.tsv",header=TRUE)
